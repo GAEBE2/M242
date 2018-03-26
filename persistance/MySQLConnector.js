8ecd46b1d0f136e606fae6d7cc84d6289f5ea8e5
GAEBE2 <gabriel.meier@edu.tbz.ch>
2018-03-26 13:43:12 +0200

    Start mysqldump and webserver

---
+++ persistance/MySQLConnector.js
@@ -0,0 +1,45 @@
+const mysql = require('mysql2');
+
+export default class MySQLConnector {
+
+    private userName;
+    private password;
+    private serverAdr;
+    private database;
+
+    private connection;
+
+    constructor(userName, password, serverAdr, database) {
+        this.userName = userName;
+        this.password = password;
+        this.database = database;
+        this.serverAdr = serverAdr;
+        this.connect();
+    }
+
+    connect() {
+        this.connection = mysql.createConnection({
+            host: this.serverAdr,
+            user: this.userName,
+            database: this.database,
+            password: this.password
+        });
+    }
+
+    private runQuery(query, params) {
+        let results;
+        this.connection.query(query, params, (err, resultRows) => {
+            if (err) {
+                throw err;
+            } else {
+                results = resultRows;
+            }
+        });
+        return results;
+    }
+
+    public getTempretatures() {
+        const query = 'SELECT * FROM temperature ORDER BY DAT DESC LIMIT 1';
+        return this.runQuery(query, null);
+    }
+}
