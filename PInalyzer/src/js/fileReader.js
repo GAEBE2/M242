6c78ef86a59c358ba11adac433326971b41a7d69
Patrick Wissiak <pwissiak@hispeed.ch>
2018-03-26 15:31:26 +0200

    Improved dashboard.

--- PInalyzer/src/js/fileReader.js
+++ PInalyzer/src/js/fileReader.js
@@ -1,18 +1,35 @@
+function setDegree(currentDegree) {
+    const min = -10, max = 40;
+    var gradient = (currentDegree >= 30) ? "linear-gradient(#F44336 0%, #FFB300 30%, #FFB300 70%, #3F51B5 100%)" :
+        "linear-gradient(#FFB300 50%, #3F51B5 100%)";
+    const perc = calculatePerc(currentDegree, min, max);
+    document.getElementById("thermometer-indicator").style.top = 100 - perc + "%";
+    document.getElementById("thermometer-indicator").style.height = perc + "%";
+    document.getElementById("thermometer-indicator").style.backgroundImage = getCssValuePrefix(gradient) + gradient;
+    document.getElementById("degree-display").innerText = currentDegree + "°C";
+}
+
+function calculatePerc(current, min, max) {
+    return ((current - min) * 100) / (max - min);
+}
+
+function setDecibel(currentDecibel) {
+    const min = 0, max = 70;
+    const perc = calculatePerc(currentDecibel, min, max);
+
+    document.getElementById("decibel-meter").style.width = perc + "%";
+    document.getElementById("decibel-display").innerText = currentDecibel + "dB";
+}
+
 function readTextFile() {
     const rawFile = new XMLHttpRequest();
     rawFile.open("GET", "../data/analyzed_data", false);
     rawFile.onreadystatechange = function () {
         if (rawFile.readyState === 4 && rawFile.status === 200 || rawFile.status === 0) {
             var allText = rawFile.responseText.split("\n");
-            const current = parseInt(allText[0]), min = -10, max = 40;
-            var gradient;
-            gradient = (current >= 30) ? "linear-gradient(#F44336 0%, #FFB300 30%, #FFB300 70%, #3F51B5 100%)" :
-                "linear-gradient(#FFB300 50%, #3F51B5 100%)";
-            const perc = ((current - min) * 100) / (max - min);
-            document.getElementById("thermometer-indicator").style.top = 100 - perc + "%";
-            document.getElementById("thermometer-indicator").style.height = perc + "%";
-            document.getElementById("thermometer-indicator").style.backgroundImage = getCssValuePrefix(gradient) + gradient;
-            document.getElementById("degree-display").innerText = current + "°C";
+            const currentDegree = parseInt(allText[0]), currentDecibel = parseInt(allText[1]);
+            setDegree(currentDegree);
+            setDecibel(currentDecibel);
         }
     };
     rawFile.send(null);
